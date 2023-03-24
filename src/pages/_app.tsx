2ca801775b4debd7e5ac16f8da8460cf61381c6c
savio <saviocarvalho01@hotmail.com>
2023-03-24 14:54:42 -0300

    temp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { globalStyles } from './styles/global'
+import logo from '../assets/logo.svg';
+import {
+  Container,
+  Header
+} from '../styles/pages/app';
+import Image from 'next/image';
 
+globalStyles()
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logo} alt=""/>
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
